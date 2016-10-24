faa3bd240fde7d7ef65ceca0e5e64af880648358
Laurent34 <laurent.esperance@gmail.com>
2016-10-24 12:07:48 +0200

    ADD toggle button

--- app/src/main/java/laurentesp/list_exercice/MainActivity.java
+++ app/src/main/java/laurentesp/list_exercice/MainActivity.java
@@ -1,44 +1,41 @@
 package laurentesp.list_exercice;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import laurentesp.list_exercice.flickr.business.OnFlickrResponseListener;
-import laurentesp.list_exercice.flickr.photo.PhotoSimple;
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+import org.honorato.multistatetogglebutton.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements ListPhotosFragment.OnHeadlineSelectedListener{
     static String mCurrentUrl = "";
     static String mCurrentTitle = "";
     public final static String EXTRA_MESSAGE = "laurentesp.list_exercice.MESSAGE";
+    private final String TAG = "Toggle_Button";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MultiStateToggleButton buttonToggle = (MultiStateToggleButton) this.findViewById(R.id.mstb_multi_id);
+        buttonToggle.setOnValueChangedListener(new ToggleButton.OnValueChangedListener()
+        {
+            @Override
+            public void onValueChanged(int position) {
+                Log.d(TAG, "Position: " + position);
+            }
+        });
     }
+
+
 
     public void onPhotoSelected(String photoTitle, String photoUrl) {
         // The user selected a photo from the photo list
 
         // Capture the photo detail fragment from the activity layout
         PhotoDetailsFragment photoDetailsFragment = (PhotoDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.frag_detail);
-
-
+        
         if ((photoDetailsFragment != null) && (photoDetailsFragment.isInLayout())) {
             // If article frag is available, we're in two-pane layout...
             // Call a method in the ArticleFragment to update its content
